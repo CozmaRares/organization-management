72764e9621a60709e5da56b11658f7f41bf27caa
Cozma Rares <cozmarares11@gmail.com>
2024-07-05 10:49:17 +0300

    initial commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body class="bg-black text-white min-h-full">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+
+</html>
