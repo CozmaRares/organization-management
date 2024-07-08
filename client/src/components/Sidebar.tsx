0c1fa32163803186ecc6ead303aa1c054ac6d5ff
Cozma Rares <cozmarares11@gmail.com>
2024-07-08 13:58:12 +0300

    feat: add tanstack/router

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>Furnizori</li>
+          <li>Clienti</li>
+          <li>Facturi</li>
+          <li>Cheltuieli</li>
+          <li>Task-uri</li>
+          <li>Rapoarte</li>
+          <li>Salarii</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
