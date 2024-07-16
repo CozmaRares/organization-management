1a3c1b500ebcd38541b3aab53992a6685d9b980c
Cozma Rares <cozmarares11@gmail.com>
2024-07-16 22:23:41 +0300

    feat(server): add server side routing

--- html/index.php
+++ html/index.php
@@ -5,29 +5,25 @@ require "../server/bootstrap.php";
 use Server\DAO\EmployeeDAO;
 use Server\Database\ConnectionFactory;
 use Server\Database\Query\SelectQueryBuilder;
+use Bramus\Router\Router;
 
-use function Server\Database\Query\_and;
-use function Server\Database\Query\_eq;
+$router = new Router();
 
-$conn = ConnectionFactory::newConnection();
+$router->get("/test", function () {
+    $conn = ConnectionFactory::newConnection();
 
-$builder = (new SelectQueryBuilder())
-    ->addCol(EmployeeDAO::COLUMNS["name"])
-    ->addCol(EmployeeDAO::COLUMNS["salary"])
-    ->where(
-        _and(
-            _eq(EmployeeDAO::COLUMNS["id"], "1"),
-            _eq(EmployeeDAO::COLUMNS["salary"], "75000"),
-        )
-    );
+    $builder = new SelectQueryBuilder();
 
-$e = EmployeeDAO::find($conn, $builder);
+    $e = EmployeeDAO::find($conn, $builder);
 
-foreach ($e as  $emp) {
-    echo "Name: " . $emp->getName();
-    /*echo "\nPosition: " . $emp->getPosition();*/
-    echo "\nSalary: " . $emp->getSalary();
-    echo "\n";
-}
+    foreach ($e as  $emp) {
+        echo "Name: " . $emp->getName();
+        echo "\nSalary: " . $emp->getSalary();
+        echo "\n";
+    }
 
-$conn->close();
+    $conn->close();
+});
+
+
+$router->run();
